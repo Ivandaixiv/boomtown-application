ce98a3603c750904d15ef805bddf4fe1de6a7ab5
Ivan <ivandaixivwork@gmail.com>
2020-01-28 14:23:05 -0800

    Adds queries for client side

--- client/src/apollo/queries.js
+++ client/src/apollo/queries.js
@@ -37,11 +37,16 @@ export const ALL_ITEMS_QUERY = gql`
 
 export const ALL_USER_ITEMS_QUERY = gql`
   query user($id: ID!) {
-    user ( id : $id) {
+    id
+    bio
+    email
+    fullname
+    items {
       ...ItemFields
     }
-    # @TODO: Query the bio, email, fullname, items, and borrowed for the user by id
-    # Use the ItemFields fragment for the items and borrowed fields.
+    borrowed {
+      ...ItemFields
+    }
   }
   ${ItemFields}
 `;
@@ -57,6 +62,9 @@ export const ALL_TAGS_QUERY = gql`
 
 export const ADD_ITEM_MUTATION = gql`
   mutation addItem($item: NewItemInput!) {
+    addItem(item: $item) {
+      id
+    }
     # @TODO: Pass the item and image into the addItem mutation as arguments
     # and return the new item id when the mutation is complete.
   }
@@ -68,25 +76,31 @@ export const ADD_ITEM_MUTATION = gql`
 
 export const VIEWER_QUERY = gql`
   query {
+    viwer {
+      id
+      email
+      fullname
+      bio
+    }
     # @TODO: Query the id, email, fullname, and bio fields for the viewer.
   }
 `;
-export const LOGOUT_MUTATION = gql`
-  mutation {
-    # @TODO: Run the logout mutation.
-  }
-`;
+// export const LOGOUT_MUTATION = gql`
+//   mutation {
+//     # @TODO: Run the logout mutation.
+//   }
+// `;
 
-export const SIGNUP_MUTATION = gql`
-  mutation signup($user: SignupInput!) {
-    # @TODO: Pass the user into the signup mutation as an argument
-    # and return the token and user id.
-  }
-`;
+// export const SIGNUP_MUTATION = gql`
+//   mutation signup($user: SignupInput!) {
+//     # @TODO: Pass the user into the signup mutation as an argument
+//     # and return the token and user id.
+//   }
+// `;
 
-export const LOGIN_MUTATION = gql`
-  mutation login($user: LoginInput!) {
-    # @TODO: Pass the user into the login mutation as an argument
-    # and return the token and user id.
-  }
-`;
+// export const LOGIN_MUTATION = gql`
+//   mutation login($user: LoginInput!) {
+//     # @TODO: Pass the user into the login mutation as an argument
+//     # and return the token and user id.
+//   }
+// `;

fdab106401227d66a0702d47285e7a0f517c7e00
Ivan <ivandaixivwork@gmail.com>
2020-03-10 16:24:36 -0700

    Adds type checking for proptypes in Share.js

--- client/src/pages/Share/Share.js
+++ client/src/pages/Share/Share.js
@@ -3,15 +3,26 @@ import { withStyles } from "@material-ui/core/styles";
 import styles from "./styles";
 import ShareForm from "../../components/ShareItemForm";
 import ShareItemPreview from "../../components/ShareItemPreview";
+import PropTypes from "prop-types";
 
 const Share = props => {
-  const { classes } = props;
+  const { classes, tags } = props;
+  console.log("Props", props);
   return (
     <div className={classes.cardView}>
       <ShareItemPreview />
-      <ShareForm tags={props.tags} />
+      <ShareForm tags={tags} />
     </div>
   );
 };
 
+Share.propTypes = {
+  classes: PropTypes.objectOf(PropTypes.string),
+  tags: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string,
+      title: PropTypes.string
+    })
+  )
+};
 export default withStyles(styles)(Share);

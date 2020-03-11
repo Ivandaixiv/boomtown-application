09d592790849fe722c427aff44df7142afcfc3c1
Ivan <ivandaixivwork@gmail.com>
2020-03-11 09:23:32 -0700

    Adds typechecking to the Items page

--- client/src/pages/Items/Items.js
+++ client/src/pages/Items/Items.js
@@ -1,12 +1,29 @@
 import React from "react";
 import ItemsGrid from "../../components/ItemsGrid";
+import PropTypes from "prop-types";
 
 const Items = ({ items }) => {
+  console.log(items);
   return (
     <div>
       <ItemsGrid items={items} />
     </div>
   );
+};
+
+Items.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string,
+      title: PropTypes.string,
+      imageurl: PropTypes.string,
+      description: PropTypes.string,
+      created: PropTypes.string,
+      tags: PropTypes.arrayOf(PropTypes.object),
+      itemowner: PropTypes.object,
+      borrower: PropTypes.object
+    })
+  )
 };
 
 export default Items;
